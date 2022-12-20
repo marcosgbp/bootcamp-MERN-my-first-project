9d6e8c4a5dd0b83fc4b607e201ed3e5365d09621
Marcos Benítez <marcbenite.mb@gmail.com>
2022-12-20 19:47:47 -0300

    Primer ejecicio React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello Dojo!</h1>
       </header>
+      <main>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt Everest</li>
+          <li>Run a Marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </main>
     </div>
   );
 }
